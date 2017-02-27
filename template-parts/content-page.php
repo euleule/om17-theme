a14e87eb697652c1a8e1d28c67c0539207c67826
Robert Krause <rokr42@gmail.com>
2017-02-27 21:50:29 +0100

    Only scroll content section

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,35 +10,37 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
-		<div class="split">
-			<div class="image-section split-section">
-				<?php
-				if (has_post_thumbnail()) {
-					the_post_thumbnail(array(600, 600));
-				} else {
-					?>
+    <div class="entry-content">
+        <div class="split">
+            <div class="image-section split-section">
+                <?php
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail(array(600, 600));
+                } else {
+                    ?>
                     <img src="<?php echo get_template_directory_uri(); ?>/images/om17-logo1.jpg">
 
                     <?php
-				}
-				?>
-			</div>
-			<div class="divider-bar"></div>
-			<div class="content-section split-section">
-<!--				<header class="entry-header">-->
-<!--					--><?php //the_title('<h1 class="entry-title">', '</h1>'); ?>
-<!--				</header><!-- .entry-header -->
+                }
+                ?>
+            </div>
+            <div class="divider-bar"></div>
+            <div class="content-section split-section">
+                <div class="scroll-container">
+                    <!--				<header class="entry-header">-->
+                    <!--					--><?php //the_title('<h1 class="entry-title">', '</h1>'); ?>
+                    <!--				</header><!-- .entry-header -->
 
-				<?php
-				the_content();
+                    <?php
+                    the_content();
 
-				wp_link_pages(array(
-					'before' => '<div class="page-links">' . esc_html__('Pages:', 'om17'),
-					'after' => '</div>',
-				));
-				?>
-			</div>
-		</div>
-	</div><!-- .entry-content -->
+                    wp_link_pages(array(
+                        'before' => '<div class="page-links">' . esc_html__('Pages:', 'om17'),
+                        'after' => '</div>',
+                    ));
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div><!-- .entry-content -->
 </article><!-- #post-## -->
