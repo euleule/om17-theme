98ef2342f72dab7046473ad0c13af54a57fb10a5
Robert Krause <rokr42@gmail.com>
2017-02-28 21:39:50 +0100

    Styled menu button

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu"
-					aria-expanded="false"><?php esc_html_e('Primary Menu', 'om17'); ?></button>
+					aria-expanded="false"><?php esc_html_e('Men&uuml;', 'om17'); ?></button>
 			<?php wp_nav_menu(array('theme_location' => 'menu-1', 'menu_id' => 'primary-menu')); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
